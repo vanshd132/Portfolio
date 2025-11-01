cfaccc420fb0247928c97a686c80556e037eab78
vansh132 <vanshji132@gmail.com>
2025-11-01 20:29:36 +0530

    customized

--- client/src/components/navigation.tsx
+++ client/src/components/navigation.tsx
@@ -10,6 +10,7 @@ export default function Navigation() {
     { href: "#home", label: "Home" },
     { href: "#about", label: "About" },
     { href: "#experience", label: "Experience" },
+    { href: "#successful-products", label: "Products" },
     { href: "#projects", label: "Projects" },
     { href: "#skills", label: "Skills" },
     { href: "#contact", label: "Contact" }
